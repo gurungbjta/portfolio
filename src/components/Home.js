c84efd7abdd61f9890cd0f49b2670f245bc86d45
gurungbjta <bijayata.gurung@sjsu.edu>
2020-09-03 00:11:51 -0700

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div className="container">
+            <div className="whiteBox ml-0 ml-sm-2 ml-md-3 ml-lg-4 ml-xl-5">
+                <div className="intro pl-4 pt-4 pr-4">
+                    <div>Welcome to my Portfolio!</div>
+                    <div>
+                    I am glad you visited. My name is Bijayata (Bjta) and I am a Computer Science Junior at San Jose State University.
+                    To see my projects on github, <a id="gitClick" href="https://github.com/gurungbjta">click here</a>.
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import {Link} from 'react-router-dom'
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div className="container pt-5">
+            <div className="text-center" id="name">BIJAYATA GURUNG</div>
+            <nav className="navbar pt-4 justify-content-around">
+                <Link to="/" className="navb">
+                    <div>Home</div>
+                </Link>
+                <Link to="/about" className="navb">
+                    <div>About Me</div>
+                </Link>
+                <Link to="/contact" className="navb">
+                    <div>Contact Me</div>
+                </Link>
+            </nav>
+        </div>
+    )
+}
