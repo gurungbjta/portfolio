abbd72634b28ed55db39035f8ac4d52d14fdce32
gurungbjta <bijayata.gurung@sjsu.edu>
2020-09-05 23:40:29 -0700

    Updates

---
+++ precache-manifest.e7aae3c488268b561ddfa0b800b18c65.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece6cb5dfc64967655367fcb859e706a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4ad67957061a290fdd34",
+    "url": "/portfolio/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "2fa5b8088581b0f00d8e",
+    "url": "/portfolio/static/css/main.82006490.chunk.css"
+  },
+  {
+    "revision": "4ad67957061a290fdd34",
+    "url": "/portfolio/static/js/2.df7449c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.df7449c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa5b8088581b0f00d8e",
+    "url": "/portfolio/static/js/main.29aba762.chunk.js"
+  },
+  {
+    "revision": "b417b3ee81e3e8f9aad5",
+    "url": "/portfolio/static/js/runtime-main.b649af6b.js"
+  },
+  {
+    "revision": "f96888a1c46bbd946e434f39580246cb",
+    "url": "/portfolio/static/media/comp.f96888a1.jpeg"
+  },
+  {
+    "revision": "f6e32aad4b549f754ad38b857abd8487",
+    "url": "/portfolio/static/media/girl2.f6e32aad.jpg"
+  },
+  {
+    "revision": "ed99e67bf9aa450254cc0f61102aff04",
+    "url": "/portfolio/static/media/girl3.ed99e67b.jpg"
+  },
+  {
+    "revision": "40244ae185def9d5f66e5f00fc8a4531",
+    "url": "/portfolio/static/media/hello.40244ae1.gif"
+  },
+  {
+    "revision": "da6fca0a817655ecf7994332fcb308e9",
+    "url": "/portfolio/static/media/pinkComp.da6fca0a.jpg"
+  },
+  {
+    "revision": "4792ec3a30b4c4ee70473a77eeef07ac",
+    "url": "/portfolio/static/media/work.4792ec3a.gif"
+  }
+]);
